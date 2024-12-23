6a0fd5edf62fb97ef6a599ac24676bfe3f948df4
PrinOrange <prinorange@outlook.com>
2024-12-23 16:07:26 +0800

    update: update readme file

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# document-search-server
+# CJK Document-Search-Server
+
+(中日韩 - 英文混写文本全文搜索服务框架 / 中日韓 - 英語混合テキスト全文検索サービスフレームワーク / 중일한 - 영어 혼합 텍스트 전문 검색 서비스 프레임워크)
+
+This is a framework for indexing sets of article and establish a server for full-text search.
+
+This project is under the construction, as far it only supports for three language patterns: Chinese-English mixed text, Japanese-English mixed text and Korean-English mixed text.
 
 To install dependencies:
 
--- lib/preprocess.ts
+++ lib/preprocess.ts
@@ -14,7 +14,7 @@ import type { Root } from "mdast";
  * @returns A new string where all sequences of whitespace are replaced with a single space.
  */
 export const compressSpaces = (text: string): string => {
-  return text.replace(/\s+/g, " ");
+	return text.replace(/\s+/g, " ");
 };
 
 /**
@@ -24,20 +24,17 @@ export const compressSpaces = (text: string): string => {
  * @param selector - The CSS selector identifying elements to remove.
  * @returns A new HTML string with the specified elements removed.
  */
-export const removeElementsBySelector = (
-  html: string,
-  selector: string
-): string => {
-  const dom = new JSDOM(html);
-  const elements = dom.window.document.querySelectorAll(selector);
+export const removeElementsBySelector = (html: string, selector: string): string => {
+	const dom = new JSDOM(html);
+	const elements = dom.window.document.querySelectorAll(selector);
 
-  elements.forEach((element) => {
-    if (element.parentNode) {
-      element.parentNode.removeChild(element);
-    }
-  });
+	elements.forEach((element) => {
+		if (element.parentNode) {
+			element.parentNode.removeChild(element);
+		}
+	});
 
-  return dom.serialize();
+	return dom.serialize();
 };
 
 /**
@@ -47,49 +44,46 @@ export const removeElementsBySelector = (
  * @param elementsToRemove - An array of MarkdownElement enums specifying which elements to remove.
  * @returns A new Markdown string with the specified elements removed.
  */
-export const removeMarkdownElements = (
-  markdown: string,
-  elementsToRemove: MarkdownElement[]
-): string => {
-  // TODO: Need to be verified.
-  const processor = unified()
-    .use(remarkParse)
-    .use(() => {
-      return (tree: Root) => {
-        visit(tree, (node, index, parent) => {
-          let nodeType: MarkdownElement | undefined;
-          switch (node.type) {
-            case "heading":
-              nodeType = MarkdownElement.heading;
-              break;
-            case "paragraph":
-              nodeType = MarkdownElement.paragraph;
-              break;
-            case "code":
-              nodeType = MarkdownElement.codeblock;
-              break;
-            case "image":
-              nodeType = MarkdownElement.image;
-              break;
-            case "link":
-              nodeType = MarkdownElement.link;
-              break;
-            case "table":
-              nodeType = MarkdownElement.table;
-              break;
+export const removeMarkdownElements = (markdown: string, elementsToRemove: MarkdownElement[]): string => {
+	// TODO: Need to be verified.
+	const processor = unified()
+		.use(remarkParse)
+		.use(() => {
+			return (tree: Root) => {
+				visit(tree, (node, index, parent) => {
+					let nodeType: MarkdownElement | undefined;
+					switch (node.type) {
+						case "heading":
+							nodeType = MarkdownElement.heading;
+							break;
+						case "paragraph":
+							nodeType = MarkdownElement.paragraph;
+							break;
+						case "code":
+							nodeType = MarkdownElement.codeblock;
+							break;
+						case "image":
+							nodeType = MarkdownElement.image;
+							break;
+						case "link":
+							nodeType = MarkdownElement.link;
+							break;
+						case "table":
+							nodeType = MarkdownElement.table;
+							break;
 						// TODO: To add more supported elements in future.
-          }
-          if (nodeType !== undefined && elementsToRemove.includes(nodeType)) {
-            if (parent && index == -1) {
-              parent.children.splice(index, 1);
-            }
-          }
-        });
-      };
-    })
-    .use(remarkStringify);
+					}
+					if (nodeType !== undefined && elementsToRemove.includes(nodeType)) {
+						if (parent && index == -1) {
+							parent.children.splice(index, 1);
+						}
+					}
+				});
+			};
+		})
+		.use(remarkStringify);
 
-  return processor.processSync(markdown).toString();
+	return processor.processSync(markdown).toString();
 };
 
 /**
@@ -99,14 +93,14 @@ export const removeMarkdownElements = (
  * @returns A string with Markdown formatting removed and spaces compressed.
  */
 export const removeMarkdownFormat = (markdown: string) => {
-  const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
-  const series: string[] = [];
+	const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
+	const series: string[] = [];
 
-  visit(mdast, "text", (node) => {
-    series.push(node.value);
-  });
+	visit(mdast, "text", (node) => {
+		series.push(node.value);
+	});
 
-  return compressSpaces(series.join(" "));
+	return compressSpaces(series.join(" "));
 };
 
 /**
@@ -116,6 +110,6 @@ export const removeMarkdownFormat = (markdown: string) => {
  * @returns A string with HTML tags removed and spaces compressed.
  */
 export const removeHTMLFormat = (html: string) => {
-  const dom = new JSDOM(html);
-  return compressSpaces(dom.window.document.body.textContent || "");
+	const dom = new JSDOM(html);
+	return compressSpaces(dom.window.document.body.textContent || "");
 };
