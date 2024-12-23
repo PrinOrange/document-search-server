df8623d5e1b5d9dee50ce3520edf6bfa51b0daab
PrinOrange <prinorange@outlook.com>
2024-12-23 10:35:07 +0800

    chores: add html preprocessor.

--- lib/preprocess.ts
+++ lib/preprocess.ts
@@ -14,7 +14,30 @@ import type { Root } from "mdast";
  * @returns A new string where all sequences of whitespace are replaced with a single space.
  */
 export const compressSpaces = (text: string): string => {
-	return text.replace(/\s+/g, " ");
+  return text.replace(/\s+/g, " ");
+};
+
+/**
+ * Removes elements from an HTML string based on a CSS selector.
+ *
+ * @param html - The HTML string to process.
+ * @param selector - The CSS selector identifying elements to remove.
+ * @returns A new HTML string with the specified elements removed.
+ */
+export const removeElementsBySelector = (
+  html: string,
+  selector: string
+): string => {
+  const dom = new JSDOM(html);
+  const elements = dom.window.document.querySelectorAll(selector);
+
+  elements.forEach((element) => {
+    if (element.parentNode) {
+      element.parentNode.removeChild(element);
+    }
+  });
+
+  return dom.serialize();
 };
 
 /**
@@ -24,45 +47,49 @@ export const compressSpaces = (text: string): string => {
  * @param elementsToRemove - An array of MarkdownElement enums specifying which elements to remove.
  * @returns A new Markdown string with the specified elements removed.
  */
-export const removeMarkdownElements = (markdown: string, elementsToRemove: MarkdownElement[]): string => {
+export const removeMarkdownElements = (
+  markdown: string,
+  elementsToRemove: MarkdownElement[]
+): string => {
   // TODO: Need to be verified.
   const processor = unified()
-		.use(remarkParse)
-		.use(() => {
-			return (tree: Root) => {
-				visit(tree, (node, index, parent) => {
-					let nodeType: MarkdownElement | undefined;
-					switch (node.type) {
-						case "heading":
-							nodeType = MarkdownElement.heading;
-							break;
-						case "paragraph":
-							nodeType = MarkdownElement.paragraph;
-							break;
-						case "code":
-							nodeType = MarkdownElement.codeblock;
-							break;
-						case "image":
-							nodeType = MarkdownElement.image;
-							break;
-						case "link":
-							nodeType = MarkdownElement.link;
-							break;
-						case "table":
-							nodeType = MarkdownElement.table;
-							break;
-					}
-					if (nodeType !== undefined && elementsToRemove.includes(nodeType)) {
-						if (parent && index == -1) {
-							parent.children.splice(index, 1);
-						}
-					}
-				});
-			};
-		})
-		.use(remarkStringify);
+    .use(remarkParse)
+    .use(() => {
+      return (tree: Root) => {
+        visit(tree, (node, index, parent) => {
+          let nodeType: MarkdownElement | undefined;
+          switch (node.type) {
+            case "heading":
+              nodeType = MarkdownElement.heading;
+              break;
+            case "paragraph":
+              nodeType = MarkdownElement.paragraph;
+              break;
+            case "code":
+              nodeType = MarkdownElement.codeblock;
+              break;
+            case "image":
+              nodeType = MarkdownElement.image;
+              break;
+            case "link":
+              nodeType = MarkdownElement.link;
+              break;
+            case "table":
+              nodeType = MarkdownElement.table;
+              break;
+						// TODO: To add more supported elements in future.
+          }
+          if (nodeType !== undefined && elementsToRemove.includes(nodeType)) {
+            if (parent && index == -1) {
+              parent.children.splice(index, 1);
+            }
+          }
+        });
+      };
+    })
+    .use(remarkStringify);
 
-	return processor.processSync(markdown).toString();
+  return processor.processSync(markdown).toString();
 };
 
 /**
@@ -72,14 +99,14 @@ export const removeMarkdownElements = (markdown: string, elementsToRemove: Markd
  * @returns A string with Markdown formatting removed and spaces compressed.
  */
 export const removeMarkdownFormat = (markdown: string) => {
-	const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
-	const series: string[] = [];
+  const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
+  const series: string[] = [];
 
-	visit(mdast, "text", (node) => {
-		series.push(node.value);
-	});
+  visit(mdast, "text", (node) => {
+    series.push(node.value);
+  });
 
-	return compressSpaces(series.join(" "));
+  return compressSpaces(series.join(" "));
 };
 
 /**
@@ -89,6 +116,6 @@ export const removeMarkdownFormat = (markdown: string) => {
  * @returns A string with HTML tags removed and spaces compressed.
  */
 export const removeHTMLFormat = (html: string) => {
-	const dom = new JSDOM(html);
-	return compressSpaces(dom.window.document.body.textContent || "");
+  const dom = new JSDOM(html);
+  return compressSpaces(dom.window.document.body.textContent || "");
 };
